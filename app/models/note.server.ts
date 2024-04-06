5482db9717fff9b5fedda44ad2e7ad600228faa0
Dan Scott <me@danjscott.dev>
2024-04-07 00:48:50 +0100

    Add tag to notes

--- app/models/note.server.ts
+++ app/models/note.server.ts
@@ -11,7 +11,7 @@ export function getNote({
   userId: User["id"];
 }) {
   return prisma.note.findFirst({
-    select: { id: true, body: true, title: true },
+    select: { id: true, body: true, title: true, tag: true },
     where: { id, userId },
   });
 }
